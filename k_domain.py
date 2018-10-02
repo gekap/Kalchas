b6936334102fb1ed466886be7551d8f73cb3cd5d
Georgios Kapellakis <anddeveloper008@gmail.com>
2018-10-02 16:18:43 -0400

    Add files via upload
    Bug fixed on unknown domains.

--- k_domain.py
+++ k_domain.py
@@ -5,14 +5,20 @@
 
 import socket
 import k_ip
+import sys
 
 
 def convert_domain(domain):
-    getIP = socket.gethostbyname(domain)
+    try:
+        getIP = socket.gethostbyname(domain)
 
-    if getIP == "0.0.0.0":
-        return False
-    else:
-        ipvResult = k_ip.validate_ipaddress(getIP)
-        return getIP
-
+        if getIP == "0.0.0.0":
+            return False
+            sys.exit(0)
+        else:
+            ipvResult = k_ip.validate_ipaddress(getIP)
+            return getIP
+    except socket.gaierror:
+        print ("Domain "+domain+" cannot resolv\n")
+        print ("It can be behind firewall, or register on private DNS")
+        sys.exit(0)
